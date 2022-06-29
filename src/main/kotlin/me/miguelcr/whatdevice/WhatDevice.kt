c72ec20e2fd1b0d1111d7c4ab7963d0bcbe29e05
MiguelX413 <lbpgaming27@gmail.com>
2022-06-29 15:27:34 -0700

    Add /device

--- src/main/kotlin/me/miguelcr/whatdevice/WhatDevice.kt
+++ src/main/kotlin/me/miguelcr/whatdevice/WhatDevice.kt
@@ -1,10 +1,14 @@
 package me.miguelcr.whatdevice
 
+import me.miguelcr.whatdevice.commands.DeviceCommand
 import org.bukkit.plugin.java.JavaPlugin
+import org.geysermc.floodgate.api.FloodgateApi
 
 class WhatDevice : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
+        val floodgateApi = FloodgateApi.getInstance()
+        getCommand("device")?.setExecutor(DeviceCommand(floodgateApi))
     }
 
     override fun onDisable() {
---
+++ src/main/kotlin/me/miguelcr/whatdevice/commands/DeviceCommand.kt
@@ -0,0 +1,26 @@
+package me.miguelcr.whatdevice.commands
+
+import org.bukkit.Bukkit
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import org.geysermc.floodgate.api.FloodgateApi
+
+class DeviceCommand(val floodgateApi: FloodgateApi) : CommandExecutor {
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        val player: Player = if (args.isEmpty()) {
+            if (sender is Player) {
+                sender
+            } else {
+                return false
+            }
+        } else {
+            Bukkit.getServer().getPlayerExact(args[0]) ?: sender.sendMessage("Could not find player"); return false
+        }
+
+        sender.sendMessage(floodgateApi.getPlayer(player.uniqueId)?.deviceOs?.name ?: "Java")
+        return true
+    }
+}
